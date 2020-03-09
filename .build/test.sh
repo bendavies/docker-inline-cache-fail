7d180a6d5ec061c28c3f888cb1c63c705253ac8d
Ben Davies <ben.davies@gmail.com>
2020-03-09 20:29:53 +0000

    mode=max

--- .build/test.sh
+++ .build/test.sh
@@ -20,8 +20,8 @@ function build {
 
   docker buildx build \
     --progress plain \
-    --cache-to=type=inline \
-    --cache-from=type=registry,ref="$tag" \
+    --cache-to type=inline,mode=max \
+    --cache-from type=registry,ref="$tag" \
     --target php \
     --tag "$tag" \
     --push \
